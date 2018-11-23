a25c849b8de6943ec96afb0d39b1a31496290fa5
unknown <pcassinga@gmail.com>
2018-11-23 09:55:02 -0800

    First commit with migration of users table

---
+++ database/migrations/2018_11_22_171945_add_numeromatricule_field_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNumeromatriculeFieldToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            
+            $table->integer('numeromatricule')->unsigned()->nullable()->default(12);
+            
+        });
+        
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            Schema::dropIfExists('numeromatricule');
+        });
+    }
+}
